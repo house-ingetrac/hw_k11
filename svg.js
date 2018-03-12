ca1a2f01a12fd5cba4bd870aed31bfc25fe2aaba
sngo1 <sngo1@stuy.edu>
2018-03-11 21:29:08 -0400

    Attempted assignment; not working

--- svg.js
+++ svg.js
@@ -1,43 +1,16 @@
 /*
 Alessandro Cartegni, Samantha Ngo
-Team Gnocchi
+Team lesBounce
 SoftDev2 pd07
-K10 -- Objectification
-2018-03-08
+K11 -- All That Bouncin'
+2018-03-11
 */
 
 var container = document.getElementById("vimage");
 var clearButton = document.getElementById("clear");
-
-var circleClicked = function(e){
-    console.log("THIS: ", this);
-    if(this.fill == "orange"){
-	this.fill = "lightblue";
-	this.setAttribute("fill", this.fill);
-	console.log("Color changed.");
-    } else {
-	container.removeChild(this);
-	red = true;
-	var obj = drawDot(Math.round(Math.random()*501), Math.round(Math.random()*501));
-	obj.display();
-	console.log("New circle drawn.");
-    }
-}
+var allDots = [];
 
 /*
-var drawDot = function(x, y){
-    var dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    dot.setAttribute("cx", x);
-    dot.setAttribute("cy", y);
-    dot.setAttribute("fill", "red");
-    dot.setAttribute("r", "25");
-    dot.addEventListener("click", circleClicked);
-    container.appendChild(dot);
-    console.log("---- Dot: ", dot);
-    return dot;
-}
-*/
-
 var drawDot = function(x, y){
     var dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     dot.xCor = x;
@@ -49,38 +22,104 @@ var drawDot = function(x, y){
 	dot.setAttribute("cy", dot.yCor);
 	dot.setAttribute("fill", dot.fill);
 	dot.setAttribute("r", dot.radius);
-	dot.addEventListener("click", circleClicked);
 	container.appendChild(dot);
+	allDots[allDots.length] = dot;
+	console.log(allDots);
     }
     console.log("---- Dot: ", dot);
     return dot;
 }
-	
+*/
+
+var runAnimation = function(obj){
+     // Delete last circle drawn -- stop motion animation
+    //clearBoard();SAVE FOR UPDATING ALL DOTS FUNCTION 
+    console.log("OBJ: ", obj);
+    // Every time a coordinate reaches an edge, reverse it to go in the opposite direction
+    if (obj.xCor >= 500 || obj.xCor <= 0){
+	obj.xAdd *= -1;
+    }
+    if (obj.yCor >= 500 || obj.yCor <= 0){
+	obj.yAdd *= -1;
+    }
+
+    // Update coordinates accordingly
+    obj.xCor += obj.xAdd;
+    obj.yCor += obj.yAdd;
+
+    // Draw new circle at coordinate
+    obj.update(obj.xCor, obj.yCor);
+    requestID = window.requestAnimationFrame(runAnimation);
+    console.log("REQID: ", obj.requestID);
+    return true;
+}
+
+var createDot = function(){
+    var dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    dot.xCor = Math.round(Math.random() * 501);
+    dot.yCor = Math.round(Math.random() * 501);
+    dot.fill = "orange";
+    dot.radius = "20";
+    dot.requestID = -1;
+    dot.xAdd = 1;
+    dot.yAdd = 1;
+    dot.start = function(){
+	dot.setAttribute("cx", dot.xCor);
+	dot.setAttribute("cy", dot.yCor);
+	dot.setAttribute("fill", dot.fill);
+	dot.setAttribute("r", dot.radius);
+	container.appendChild(dot);
+	allDots[allDots.length] = dot;
+	console.log(allDots);
+	console.log("Animation starting...");
+	console.log("THIS: ", this);
+	console.log("DOT: ", dot);
+	runAnimation(dot);
+    }
+    dot.update = function(x,y){
+	console.log("Updating dot location...");
+	dot.setAttribute("cx", x);
+	dot.xCor = x;
+	dot.setAttribute("cy", y);
+	dot.yCor = y;
+	dot.setAttribute("fill", dot.fill);
+	dot.setAttribute("r", dot.radius);
+	console.log(allDots);
+	console.log("Dot location updated.");
+    }
+    dot.stop = function(){
+	console.log("Cancelling ID: ", dot.requestID);
+	window.cancelAnimationFrame(dot.requestID);
+	console.log("...Animation stopped.");
+	return true;
+    }
+    console.log("Creating dot...");
+    dot.start();
+    console.log("Created Dot: ", dot);
+    return dot;
+}
+
+/*
 var canvasClicked = function(e){
-    /*
-      console.log("clicked");
-      console.log("e:");
-      console.log(e);*/
     if(e.target == this){
 	var obj = drawDot(e.offsetX, e.offsetY);
 	obj.display();
 	console.log("drawn");
     }
 };
+*/
 
 //Remove all the inner nodes of the SVG area
 var clear = function(e){
-    console.log("Clear");
+    console.log("Clearing...");
     var firstChild = container.firstChild;
     while(firstChild){
-	console.log("Clearing: ");
-	console.log(firstChild);
 	container.removeChild(firstChild);
 	firstChild = container.firstChild;
     }
+    console.log("Board cleared.");
     first = true;
 }
 
-	
-container.addEventListener("click", canvasClicked);
+container.addEventListener("click", createDot);
 clearButton.addEventListener("click", clear);
